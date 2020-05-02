37ef9e7dcd9b6af772090f1a36d839de3310bbaa
Pablo Baeyens <pbaeyens31+github@gmail.com>
2020-05-02 23:44:09 +0200

    Fixes regression Lists and ratings are exposed through different PHP functions.

--- faLetterboxd/README.md
+++ faLetterboxd/README.md
@@ -2,22 +2,39 @@
 
 Generates csv file compatible with Letterboxd diary importer from Filmaffinity user's data given their id.
 
-## How to get your id
+## How to get your user id
 
 Go to your profile page and copy the `user_id` field from the url:
 
 `https://www.filmaffinity.com/es/userratings.php?user_id=`**XXXXXX**
 
+## How to get your list id
+
+Go to the list pages (in the left menu), and access the list you want (need to be public).
+
+You need to copy the `list_id` field from the url:
+
+`https://www.filmaffinity.com/es/mylist.php?list_id=`**XXXXXX**
+
 ## Running the script
 
-You need to have [Python 3](https://www.python.org/downloads) and [BeautifulSoup 4](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup) installed. Once installed just run:
+You need to have [Python 3](https://www.python.org/downloads), [BeautifulSoup 4](https://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-beautiful-soup), [requests](https://requests.readthedocs.io/en/master/) and [lxml](https://lxml.de/) installed. Once installed just run:
 
 ``` sh
 ./faScrap.py [--csv FILE] [--lang LANG] id
 ```
 
+### With docker
+
+First of all you need to have docker installed, and latter you have to run this simple steps
+
+You need to replace `USER_ID` with your current user id
+
+`docker run --name fa-letterboxd fa-letterboxd python3 faScrap.py --lang en --csv fa-to-lbx.csv USER_ID && docker cp fa-letterboxd:/fa-to-lbx.csv . && docker rm fa-letterboxd`
+
 ## Options
 
+- `--list LIST` The id of the public list you wanna export, if is available export the list and not your ratings
 - `--csv FILE` sets csv export file name to `FILE`
 - `--lang LANG` sets language to `LANG`. Letterboxd importer works best with english, the default option.
 
--- faLetterboxd/faScrap.py
+++ faLetterboxd/faScrap.py
@@ -5,161 +5,200 @@
 # for usage info and options
 
 import argparse
-import requests
 import csv
-import bs4
-
+import sys
 from datetime import datetime
 import platform
 import locale
 
+import requests
+import bs4
+
+
+# FilmAffinity root URL
+FA_ROOT_URL = "https://www.filmaffinity.com/{lang}/"
+
 
 def set_locale(lang):
-  """Attempts to set locale."""
-  
-  if platform.system() in {"Linux", "Darwin"}:
-    loc = "es_ES.utf8" if lang == "es" else "en_US.utf8"
-  elif platform.system() == "Windows":
-    loc = "es-ES" if lang == "es" else "en-US"
-  else:
-    raise locale.Error()
-  
-  locale.setlocale(locale.LC_ALL, loc)
+    """Attempts to set locale."""
+
+    if platform.system() in {"Linux", "Darwin"}:
+        loc = "es_ES.utf8" if lang == "es" else "en_US.utf8"
+    elif platform.system() == "Windows":
+        loc = "es-ES" if lang == "es" else "en-US"
+    else:
+        raise locale.Error()
+
+    locale.setlocale(locale.LC_ALL, loc)
 
 
 def get_date(tag, lang):
-  """Gets date from tag (format YYYY-MM-DD)"""
-  
-  if lang == "es":
-    date_str = tag.string[len("Votada el día: "):].strip()
-    fecha = datetime.strptime(date_str, "%d de %B de %Y").date()
-  else:
-    date_str = tag.string[len("Rated on "):].strip()
-    fecha = datetime.strptime(date_str, "%B %d, %Y").date()
-  
-  return fecha.strftime("%Y-%m-%d")
+    """Gets date from tag (format YYYY-MM-DD)"""
+
+    if lang == "es":
+        date_str = tag.string[len("Votada el día: ") :].strip()
+        fecha = datetime.strptime(date_str, "%d de %B de %Y").date()
+    else:
+        date_str = tag.string[len("Rated on ") :].strip()
+        fecha = datetime.strptime(date_str, "%B %d, %Y").date()
+
+    return fecha.strftime("%Y-%m-%d")
 
 
 def get_directors(tag):
-  """Gets directors from a film"""
-  directors = list(
-    map(
-      lambda d: d.a["title"],
-      tag.find_all(class_ = "mc-director")[0].find_all(class_ = "nb")))
-  
-  for director in directors:
-    if director.endswith("(Creator)"):
-      director = director[:-10]
-  
-  return ", ".join(directors)
+    """Gets directors from a film"""
+
+    def sanitize_director_tag(d):
+        """Sanitizes director tag into director name."""
+        director = d.a["title"]
+        return director[:-10] if director.endswith("(Creator)") else director
+
+    return ", ".join(
+        sanitize_director_tag(d)
+        for d in tag.find_all(class_="mc-director")[0].find_all(class_="nb")
+    )
 
 
 def is_film(tag, lang):
-  """Checks if given tag is a film"""
-  
-  title = tag.find_all(class_ = "mc-title")[0].a.string.strip()
-  skip = []
-  
-  if lang == "es":
-    skip = ["(Serie de TV)", "(Miniserie de TV)", "(TV)", "(C)"]
-  else:
-    skip = ["(TV Series)", "(TV Miniseries)", "(TV)", "(S)"]
-  
-  return not any(map(title.endswith, skip))
+    """Checks if given tag is a film"""
 
+    title = tag.find_all(class_="mc-title")[0].a.string.strip()
 
-def get_data(user_id, lang):
-  """Gets list of films from user id"""
-  
-  data = []
-  eof = False
-  n = 1
-  FA = "https://www.filmaffinity.com/" + lang + \
-      "/userratings.php?user_id={id}&p={n}&orderby=4"
-  
-  while not eof:
-    request = requests.get(FA.format(id = user_id, n = n))
-    request.encoding = "utf-8"
-    page = bs4.BeautifulSoup(request.text, "lxml")
-    tags = page.find_all(
-      class_ = ["user-ratings-header", "user-ratings-movie"])
-    cur_date = None
-    
-    for tag in tags:
-      if tag["class"] == ["user-ratings-header"]:
-        cur_date = get_date(tag, lang)
-      elif is_film(tag, lang):
-        title = tag.find_all(class_ = "mc-title")[0].a
-        film = {
-          "Title": title.string.strip(),
-          "Year": title.next_sibling.strip()[1:-1],
-          "Directors": get_directors(tag),
-          "WatchedDate": cur_date,
-          "Rating": int(tag.find_all(class_ = "ur-mr-rat")[0].string)/2,
-          "Rating10": tag.find_all(class_ = "ur-mr-rat")[0].string
-        }
-        data.append(film)
-    
-    eof = request.status_code != 200
-    if not eof:
-      print("Página {n}".format(n = n), end = "\r")
+    if lang == "es":
+        skip = ["(Serie de TV)", "(Miniserie de TV)", "(TV)", "(C)"]
     else:
-      print("Página {n}. Download complete!".format(n = n - 1))
-    
-    n += 1
-  
-  return data
+        skip = ["(TV Series)", "(TV Miniseries)", "(TV)", "(S)"]
+
+    return not any(title.endswith(suffix) for suffix in skip)
 
 
-def save_to_csv(data, filename):
-  """Saves list of dictionaries in a csv file"""
-  
-  with open(filename, 'w', newline = '') as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(list(data[0]))
-    
-    for film in data:
-      writer.writerow(list(film.values()))
+def pages_from(template):
+    """Yields pages from a given section until one of them fails."""
+
+    eof = False
+    n = 1
+
+    while not eof:
+        request = requests.get(template.format(n))
+        request.encoding = "utf-8"
+        yield bs4.BeautifulSoup(request.text, "lxml")
+
+        eof = request.status_code != 200
+        if not eof:
+            print("Página {n}".format(n=n), end="\r")
+        else:
+            print("Página {n}. Download complete!".format(n=n - 1))
+        n += 1
+
+
+def get_profile_data(user_id, lang):
+    """Yields films rated by user given user id"""
+
+    FA = (FA_ROOT_URL + "userratings.php?user_id={id}&p={{}}&orderby=4").format(
+        lang=lang, id=user_id
+    )
+
+    for page in pages_from(FA):
+        tags = page.find_all(class_=["user-ratings-header", "user-ratings-movie"])
+        cur_date = None
+
+        for tag in tags:
+            if tag["class"] == ["user-ratings-header"]:
+                cur_date = get_date(tag, lang)
+            elif is_film(tag, lang):
+                title = tag.find_all(class_="mc-title")[0].a
+                yield {
+                    "Title": title.string.strip(),
+                    "Year": title.next_sibling.strip()[1:-1],
+                    "Directors": get_directors(tag),
+                    "WatchedDate": cur_date,
+                    "Rating": int(tag.find_all(class_="ur-mr-rat")[0].string) / 2,
+                    "Rating10": tag.find_all(class_="ur-mr-rat")[0].string,
+                }
+
+
+def get_list_data(user_id, list_id, lang):
+    """Yields films from list given list id"""
+
+    FA = (
+        FA_ROOT_URL + "userlist.php?user_id={user_id}&list_id={list_id}&page={{}}"
+    ).format(lang=lang, user_id=user_id, list_id=list_id)
+
+    for page in pages_from(FA):
+        tags = page.find_all(class_=["movie-wrapper"])
+
+        for tag in tags:
+            title = tag.find_all(class_="mc-title")[0].a
+            yield {
+                "Title": title.string.strip(),
+                "Year": title.next_sibling.strip()[1:-1],
+                "Directors": get_directors(tag),
+            }
+
+
+def save_to_csv(films, filename):
+    """Saves films in a csv file"""
+
+    with open(filename, "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(list(next(films)))
+
+        for film in films:
+            writer.writerow(list(film.values()))
 
 
 if __name__ == "__main__":
-  parser = argparse.ArgumentParser(
-    description =
-    "Generates csv compatible with LetterBoxd from Filmaffinity user's id.")
-  parser.add_argument("id", help = "User's id")
-  parser.add_argument(
-    "--csv", nargs = 1, help = "Name of export FILE", metavar = "FILE")
-  parser.add_argument(
-    "--lang",
-    nargs = 1,
-    help = "Language for exporting",
-    metavar = "LANG",
-    default = ["en"],
-    choices = {"es", "en"})
-  
-  args = parser.parse_args()
-  export_file = args.csv[
-    0] if args.csv else "filmAffinity_{lang}_{id}.csv".format(
-      id = args.id, lang = args.lang[0])
-  
-  try:
-    set_locale(args.lang[0])
-  except locale.Error:
-    print(
-      "Could not set locale for \'{lang}\' and UTF-8 encoding.".format(
-        lang = args.lang[0]))
-    manual_locale = input("locale (empty for default): ").strip()
-    if manual_locale:
-      try:
-        locale.setlocale(locale.LC_ALL, manual_locale)
-      except locale.Error as e:
-        print(e)
-        exit()
-  
-  try:
-    data = get_data(args.id, args.lang[0])
-  except ValueError as v:
-    print("Error:", v)
-    exit()
-  
-  save_to_csv(data, export_file)
+    parser = argparse.ArgumentParser(
+        description="Generates Letterboxd-compatible csv from Filmaffinity user data."
+    )
+    parser.add_argument("id", help="User id")
+    parser.add_argument("--list", help="List id", metavar="LIST")
+    parser.add_argument("--csv", nargs=1, help="Name of export FILE", metavar="FILE")
+    parser.add_argument(
+        "--lang",
+        nargs=1,
+        help="Language for exporting",
+        metavar="LANG",
+        default=["en"],
+        choices={"es", "en"},
+    )
+
+    args = parser.parse_args()
+
+    if args.csv:
+        export_file = args.csv[0]
+    elif args.list:
+        export_file = "filmAffinity_{lang}_{id}_list_{list_id}.csv".format(
+            id=args.id, lang=args.lang[0], list_id=args.list
+        )
+    else:
+        export_file = "filmAffinity_{lang}_{id}.csv".format(
+            id=args.id, lang=args.lang[0]
+        )
+
+    try:
+        set_locale(args.lang[0])
+    except locale.Error:
+        print(
+            "Could not set locale for '{lang}' and UTF-8 encoding.".format(
+                lang=args.lang[0]
+            )
+        )
+        manual_locale = input("locale (empty for default): ").strip()
+        if manual_locale:
+            try:
+                locale.setlocale(locale.LC_ALL, manual_locale)
+            except locale.Error as e:
+                print(e)
+                sys.exit()
+
+    try:
+        if args.list:
+            data = get_list_data(args.id, args.list, args.lang[0])
+        else:
+            data = get_profile_data(args.id, args.lang[0])
+    except ValueError as v:
+        print("Error:", v)
+        sys.exit()
+
+    save_to_csv(data, export_file)
