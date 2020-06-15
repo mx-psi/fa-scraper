f6f11339622daceedb4a9b042cf1b080110c971f
Pablo Baeyens <pbaeyens31+github@gmail.com>
2020-06-15 22:19:49 +0200

    Initial poetry version

--- fa_scrapper/fa_scrapper.py
+++ fa_scrapper/fa_scrapper.py
@@ -14,9 +14,8 @@
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-import argparse
 import csv
-import sys
+
 from datetime import datetime
 import platform
 import locale
@@ -155,60 +154,3 @@ def save_to_csv(films, filename):
 
         for film in films:
             writer.writerow(list(film.values()))
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="Generates Letterboxd-compatible csv from Filmaffinity user data."
-    )
-    parser.add_argument("id", help="User id")
-    parser.add_argument("--list", help="List id", metavar="LIST")
-    parser.add_argument("--csv", nargs=1, help="Name of export FILE", metavar="FILE")
-    parser.add_argument(
-        "--lang",
-        nargs=1,
-        help="Language for exporting",
-        metavar="LANG",
-        default=["en"],
-        choices={"es", "en"},
-    )
-
-    args = parser.parse_args()
-
-    if args.csv:
-        export_file = args.csv[0]
-    elif args.list:
-        export_file = "filmAffinity_{lang}_{id}_list_{list_id}.csv".format(
-            id=args.id, lang=args.lang[0], list_id=args.list
-        )
-    else:
-        export_file = "filmAffinity_{lang}_{id}.csv".format(
-            id=args.id, lang=args.lang[0]
-        )
-
-    try:
-        set_locale(args.lang[0])
-    except locale.Error:
-        print(
-            "Could not set locale for '{lang}' and UTF-8 encoding.".format(
-                lang=args.lang[0]
-            )
-        )
-        manual_locale = input("locale (empty for default): ").strip()
-        if manual_locale:
-            try:
-                locale.setlocale(locale.LC_ALL, manual_locale)
-            except locale.Error as e:
-                print(e)
-                sys.exit()
-
-    try:
-        if args.list:
-            data = get_list_data(args.id, args.list, args.lang[0])
-        else:
-            data = get_profile_data(args.id, args.lang[0])
-    except ValueError as v:
-        print("Error:", v)
-        sys.exit()
-
-    save_to_csv(data, export_file)
---
+++ tests/test_fa_scrapper.py
@@ -0,0 +1,5 @@
+from fa_scrapper import __version__
+
+
+def test_version():
+    assert __version__ == "0.1.0"
