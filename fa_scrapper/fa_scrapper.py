49e80dcda639f428c1ac1a9ca9126a7ce3857a0b
Pablo Baeyens <pbaeyens31+github@gmail.com>
2021-11-11 19:11:55 +0100

    Add beautifulsoup4 types (#45)

--- fa_scrapper/fa_scrapper.py
+++ fa_scrapper/fa_scrapper.py
@@ -57,8 +57,11 @@ def set_locale(lang: str):
     locale.setlocale(locale.LC_ALL, loc)
 
 
-def get_date(tag, lang: str) -> str:
+def get_date(tag: bs4.element.Tag, lang: str) -> str:
     """Gets date from tag (format YYYY-MM-DD)"""
+
+    if not tag.string:
+        raise ValueError("Missing date on tag {}".format(tag))
 
     if lang == "es":
         date_str = tag.string[len("Votada el día: ") :].strip()
@@ -70,12 +73,19 @@ def get_date(tag, lang: str) -> str:
     return fecha.strftime("%Y-%m-%d")
 
 
-def get_directors(tag) -> str:
+def get_directors(tag: bs4.element.Tag) -> str:
     """Gets directors from a film"""
 
-    def sanitize_director_tag(d):
+    def sanitize_director_tag(d: bs4.element.Tag) -> str:
         """Sanitizes director tag into director name."""
+        if not d.a:
+            # No director, skip
+            return ""
+
         director = d.a["title"]
+        if isinstance(director, list):
+            director = " ".join(director)
+
         return director[:-10] if director.endswith("(Creator)") else director
 
     return ", ".join(
@@ -84,7 +94,9 @@ def get_directors(tag) -> str:
     )
 
 
-def is_chosen_category(tag, lang: str, ignore_list) -> bool:
+def is_chosen_category(
+    tag: bs4.element.Tag, lang: str, ignore_list: Iterable[FACategory]
+) -> bool:
     """Checks if given tag is within the chosen categories"""
 
     title = tag.find_all(class_="mc-title")[0].a.string.strip()
@@ -109,7 +121,7 @@ def is_chosen_category(tag, lang: str, ignore_list) -> bool:
     return not any(title.endswith(suffix) for suffix in skip)
 
 
-def pages_from(template: str):
+def pages_from(template: str) -> Iterator[bs4.BeautifulSoup]:
     """Yields pages from a given section until one of them fails."""
 
     eof = False
@@ -130,7 +142,7 @@ def pages_from(template: str):
 
 
 def get_profile_data(
-    user_id: str, lang: str, ignore_list
+    user_id: str, lang: str, ignore_list: Iterable[FACategory]
 ) -> Iterator[Mapping[str, Any]]:
     """Yields films rated by user given user id"""
 
@@ -158,7 +170,7 @@ def get_profile_data(
 
 
 def get_list_data(
-    user_id: str, list_id: str, lang: str, ignore_list: Iterable[str]
+    user_id: str, list_id: str, lang: str, ignore_list: Iterable[FACategory]
 ) -> Iterator[Mapping[str, str]]:
     """Yields films from list given list id"""
 
@@ -197,7 +209,9 @@ def get_user_lists(user_id: str, lang: str) -> Iterator[List[str]]:
             yield [list_id, list_name]
 
 
-def save_lists_to_csv(user_id: str, lang: str, pattern: str, ignore_list):
+def save_lists_to_csv(
+    user_id: str, lang: str, pattern: str, ignore_list: Iterable[FACategory]
+):
     """Extracts all lists from a user and saves them independently"""
 
     for user_list in get_user_lists(user_id, lang):
