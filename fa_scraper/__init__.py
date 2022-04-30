312af56da755ae9a86306a3ef2d4fc9fee01a584
Pablo Baeyens <pbaeyens31+github@gmail.com>
2022-05-01 01:16:12 +0200

    Expose `get_profile_data` (#79)
    * Expose `get_profile_data
    
    * Update __init__.py

--- fa_scraper/__init__.py
+++ fa_scraper/__init__.py
@@ -3,4 +3,7 @@ try:
 except ModuleNotFoundError:
     import importlib_metadata  # type: ignore
 
+from .types import UserId, Lang, FACategory
+from .fa_scraper import get_profile_data
+
 __version__ = importlib_metadata.version(__name__)
